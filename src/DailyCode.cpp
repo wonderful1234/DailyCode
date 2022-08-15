72afcafebd2ee630e8b17280ec3c7f90fa2ec9a2
wangfude <wangfude@qianxin.com>
2022-08-15 19:27:42 +0800

    add

--- src/DailyCode.cpp
+++ src/DailyCode.cpp
@@ -1,6 +1,14 @@
 ﻿
 #include <iostream>
+#include <rapidjson/document.h>
+#include <rapidjson/istreamwrapper.h>
+#include <fstream>
+using namespace rapidjson;
+using namespace std;
 int main()
 {
-
+	ifstream ifs("test.json");
+	IStreamWrapper isw(ifs);
+	Document d;
+	d.ParseStream(isw);
 }
