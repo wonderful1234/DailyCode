f69e273e971e07a800e1328bccad87b832ffc8a8
wangfude <wangfude@qianxin.com>
2022-08-19 14:42:58 +0800

    update

--- src/DailyCode.cpp
+++ src/DailyCode.cpp
@@ -1,11 +1,28 @@
 ﻿
 #include <iostream>
 #include <fstream>
+#include "WzSerialportPlus.h"
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
 using namespace std;
+void PareJsonFromFile(std::string filePath)
+{
+	std::ifstream f;
+	f.open(filePath, std::ifstream::in);
+	if (!f.good())
+		return;
+	json data = json::parse(f);
+}
+void PareJsonFromString(std::string str)
+{
+	
+}
+void revalue(int m)
+{
+
+}
 int main()
 {
-	std::ifstream f("example.json");
-	//json data = json::parse(f);
+
+
 }
